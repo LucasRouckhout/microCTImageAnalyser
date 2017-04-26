10df9908c84a08236113dabcc1eb7ab13833533f
Lucas Rouckhout <lucas.rouckhout@gmail.com>
2017-04-26 13:31:50 +0200

    First implementation of data_processing module. Still need to write get_voltage function.

--- Analyzer/data_processor.py
+++ Analyzer/data_processor.py
@@ -5,11 +5,41 @@ import pandas as pd
 import numpy as np
 
 def create_data_frame(images, positions):
-  df_main = pd.DataFrame()
-  linepairs_per_distance = [position[0] for position in positions]
   
-  df_main.set_index(linepairs_per_distance)
+  main_dict = {}
 
   for image in images:
-    break
 
+    modulations = []
+    for position in positions:
+      modulations.append(get_modulation(image, position)
+
+    main_dict[get_voltage(image)] = modulations
+  
+  return pd.DataFrame(main_dict)
+
+def get_modulation(image, position, spacing = 10):
+  
+  lines = []
+  for count in range(5):
+    lines.append(afb.Read_Image(image, position[1], position[2],
+    position[3]+count*spacing))
+  
+  def modulation(line):
+    return (max(line)-min(line))/(max(line)+min(line))
+  
+  return map(modulation, lines)
+
+  
+def get_voltage(image):
+  """
+  Given an image path this functions returns the value for the voltage
+  at which this image was taken. 
+
+  Args:
+    image (str): A string representing the path to the image file.
+
+  Returns:
+    voltage (int): An integer representing the voltage.
+  """
+  pass
