e3bcf8b4f15fd360124df6dd5de00e3214ab56c6
Lucas Rouckhout <lucas.rouckhout@gmail.com>
2017-04-27 12:10:14 +0200

    Added docstrings to the data_processing module.

--- Analyzer/data_processor.py
+++ Analyzer/data_processor.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 
 import afbeeldingen.py as afb
-import pandas as pd
 import numpy as np
 
 def create_data_dictionary(images, positions):
-  
+  """
+  This function generates a dictionary using the voltage at which an image is
+  taken as keywords. Each keyword is connected to a list containg tuples of 
+  the modulation at a given spatial frequency and the corresponding standard
+  deviation on that modulation. As we wish to plot these values later on the 
+  list of modulations is order in the same way as the spatial frequencies.
+
+  Args:
+    images (list of str): A list of strings representing the path to the image
+      files.
+    positions (list): A list of lists formatted in a specific way which contains
+      all the information on the positions and the spatial frequency at these 
+      positions. See help(get_modulations()) for specifics on the used format.
+
+  Returns:
+    main_dict (dict): The dictionary described in the main body of this docstring.
+  """
   main_dict = {}
 
   for image in images:
@@ -20,7 +35,19 @@ def create_data_dictionary(images, positions):
   return main_dict
 
 def get_modulation(image, position, spacing = 10):
-  
+  """
+  This function calculates the intensity modulation at a specific position
+  on the image. 
+
+  Args: 
+    image (str): path the image file.
+    position (list): a list containing all the information on a specifc position.
+      formatted as such [linepairs/distance, x_initial, x_final, y]
+
+  Returns:
+    A list containing the average modulation at that position and the standard 
+      deviation.
+  """
   lines = []
   for count in range(5):
     lines.append(afb.Read_Image(image, position[1], position[2],
@@ -29,7 +56,8 @@ def get_modulation(image, position, spacing = 10):
   def modulation(line):
     return (max(line)-min(line))/(max(line)+min(line))
   
-  return map(modulation, lines)
+  modulation = map(modulation, lines)
+  return [np.average(modulation), np.std(modulation)]
 
   
 def get_voltage(image):
