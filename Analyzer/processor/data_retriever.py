c53467fc7d3de9c2aaa18bcebcb0d728b617fea1
Lucas Rouckhout <lucas.rouckhout@gmail.com>
2017-05-04 11:10:20 +0200

    Some debugging

--- Analyzer/processor/data_retriever.py
+++ Analyzer/processor/data_retriever.py
@@ -6,18 +6,13 @@ def retrieve_data(images_map, position_file,is_jima, file_glob = "normalised_*.t
   """
   This functions returns a list of image paths and a list of positions.
   """
-  images = glob.glob(images_map+file_glob)
-  if not images:
-    print("Images empty!")
-  positions = retrieve_positions(position_file)
+  images = glob.glob(images_map+"/"+file_glob)
+  positions = retrieve_positions(str(position_file))
   
   return images, positions
 
 def retrieve_positions(position_file):
-  try:
-    position_lines = open(position_file, 'r').readlines()
-    positions = [float(line.split()) for line in position_lines]
-    return positions
+  position_lines = open(position_file, 'r').readlines()
+  positions = [list(map(float,line.split())) for line in position_lines]
+  return positions
 
-  except(IOError, e):
-    raise e
